446d624d1cbad084911f35aa1aa3e5d8e944988e
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-01-20 22:15:55 +0300

    Encoder of the 3D UNet implemented. There is inconsistency in the channels.
    Former-commit-id: 7b3dc08e1b1ca434a1fcbe9c8632af76b3e23beb [formerly 601322b5d6269f79338879df16bda533c4931c2f]
    Former-commit-id: 9109642fce3ae34c84d4a7d0bb2096e7430adf06

---
+++ Code/UNet.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+class UNet3D(nn.Module):
+    # Implemented from https://arxiv.org/pdf/1606.06650.pdf.
+    def __init__(self):
+        super(UNet3D, self).__init__()
+
+        self.max_pool_2x2x2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.down_conv1 = self.__double_conv(3, 32)
+        self.down_conv2 = self.__double_conv(64, 128)
+        self.down_conv3 = self.__double_conv(128, 256)
+        self.down_conv4 = self.__double_conv(256, 512)
+
+    def forward(self, image):
+        # bs, c, h, w
+        # encoder
+        x1 = self.down_conv1(image)
+        x2 = self.max_pool_2x2x2(x1)
+        x3 = self.down_conv2(x2)
+        x4 = self.max_pool_2x2x2(x3)
+        x5 = self.down_conv3(x4)
+        x6 = self.max_pool_2x2x2(x5)
+        x7 = self.down_conv3(x6)
+        x8 = self.max_pool_2x2x2(x7)
+        print(x1.size())
+        print(x3.size())
+        print(x5.size())
+        print(x7.size())
+        print(x8.size())
+
+        return x8
+
+
+    def __double_conv(self, in_channels, out_channels):
+        conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels*2, kernel_size=3),
+            nn.ReLU(inplace=True)
+        )
+
+        return conv
+
+if __name__ == "__main__":
+    image = torch.rand((1, 3, 256, 256, 256))
+    model = UNet3D()
+    print(model(image))
