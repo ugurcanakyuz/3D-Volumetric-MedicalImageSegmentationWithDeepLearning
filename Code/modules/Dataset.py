f8dec5b1e9572f26c0eed61548bfff76adfa7511
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-02-01 23:24:10 +0300

    Transform has been added.
    Former-commit-id: d241cbc6a3a8c629af97ce318e4d5215b7c6c1dd [formerly fd3c99e0c440bf9b3f5380f54679d958112f7edb]
    Former-commit-id: 482bb21dc6ca30a2596facf2b2c3c6f20c555a4c

--- Code/modules/Dataset.py
+++ Code/modules/Dataset.py
@@ -2,6 +2,7 @@ import os
 
 import nibabel as nib
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 from modules.Utils import get_file_names
@@ -9,12 +10,13 @@ from modules.Utils import get_file_names
 
 class FeTADataSet(Dataset):
     # def __init__(self, quality=[], labels=[], pathologies=[], load_3d=None):
-    def __init__(self, path="feta_2.1", train=True, pathology="all"):
+    def __init__(self, path="feta_2.1", train=True, transform=None, pathology="all"):
         """"""
 
         count_train = 70  # First 70 MRI image consist of 40 Pathological and 20 Neurotypical.
         self.__path_base = path
         self.__train = train
+        self.__transform = transform
 
         self.meta_data = pd.read_csv(os.path.join(self.__path_base, "participants.tsv"), sep="\t")
         self.__paths_file = get_file_names(self.__path_base)
@@ -48,6 +50,12 @@ class FeTADataSet(Dataset):
 
         mri_image = nib.load(path_image).get_fdata()
         mri_mask = nib.load(path_mask).get_fdata()
+
+        if self.__transform:
+            mri_image = torch.tensor(mri_image)
+            mri_image = mri_image.view(1, 256, 256, 256)
+            mri_image = self.__transform(mri_image)
+            mri_image = mri_image.view(256, 256, 256)
 
         return mri_image, mri_mask
 
