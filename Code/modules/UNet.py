8473d8d8446f97d484ce58578f188e8f5d8e0eb1
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-02-04 11:29:13 +0300

    UNet architecture has been fixed. Code refactoring.
    Former-commit-id: e77e57970c9a7ead8ed9d45caaf9e7f9687738b9 [formerly 740a19dffe3480f11dfdde023a6fc62e3ccf3781]
    Former-commit-id: 25fa9d794b04eee00680f4ed102a2bda1e8cad86

--- Code/modules/UNet.py
+++ Code/modules/UNet.py
@@ -7,6 +7,8 @@ class UNet2D(nn.Module):
     # Implemented from https://arxiv.org/pdf/1505.04597.pdf
     def __init__(self):
         super(UNet2D, self).__init__()
+        kernel_size_ = (2, 2, 2)
+        stride_ = (2, 2, 2)
 
         self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.down_conv_1 = self.__double_conv2d(1, 64)
@@ -17,36 +19,36 @@ class UNet2D(nn.Module):
 
         self.up_trans_1 = nn.ConvTranspose2d(in_channels=1024,
                                              out_channels=512,
-                                             kernel_size=2,
-                                             stride=2
+                                             kernel_size=kernel_size_,
+                                             stride=stride_
                                              )
         self.up_conv_1 = self.__double_conv2d(1024, 512)
 
         self.up_trans_2 = nn.ConvTranspose2d(in_channels=512,
                                              out_channels=256,
-                                             kernel_size=2,
-                                             stride=2
+                                             kernel_size=kernel_size_,
+                                             stride=stride_
                                              )
         self.up_conv_2 = self.__double_conv2d(512, 256)
 
         self.up_trans_3 = nn.ConvTranspose2d(in_channels=256,
                                              out_channels=128,
-                                             kernel_size=2,
-                                             stride=2
+                                             kernel_size=kernel_size_,
+                                             stride=stride_
                                              )
         self.up_conv_3 = self.__double_conv2d(256, 128)
 
         self.up_trans_4 = nn.ConvTranspose2d(in_channels=128,
                                              out_channels=64,
-                                             kernel_size=2,
-                                             stride=2
+                                             kernel_size=kernel_size_,
+                                             stride=stride_
                                              )
         self.up_conv_4 = self.__double_conv2d(128, 64)
 
         self.out = nn.Conv2d(
             in_channels=64,
             out_channels=8,
-            kernel_size=1
+            kernel_size=(1, 1, 1,)
         )
 
     def forward(self, image):
@@ -82,54 +84,59 @@ class UNet2D(nn.Module):
 
         return x
 
-    def __double_conv2d(self, in_c, out_c):
+    @staticmethod
+    def __double_conv2d(in_c, out_c, kernel_size_=(3, 3, 3)):
         conv = nn.Sequential(
-            nn.Conv2d(in_c, out_c, kernel_size=3),
+            nn.Conv2d(in_c, out_c, kernel_size=kernel_size_),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_c, out_c, kernel_size=3),
+            nn.Conv2d(out_c, out_c, kernel_size=kernel_size_),
             nn.ReLU(inplace=True),
         )
 
         return conv
 
-    def __crop_img2d(tensor, target_tensor):
+    @staticmethod
+    def __crop_img2d(source_tensor, target_tensor):
         target_size = target_tensor.size()[2]
-        tensor_size = tensor.size()[2]
+        tensor_size = source_tensor.size()[2]
         delta = tensor_size - target_size
         delta = delta // 2
 
-        return tensor[:, :, delta:tensor_size - delta, delta:tensor_size - delta]
+        return source_tensor[:, :, delta:tensor_size - delta, delta:tensor_size - delta]
 
 
 class UNet3D(nn.Module):
     # Implemented from https://arxiv.org/pdf/1606.06650.pdf.
     def __init__(self, n_classes=8):
         super(UNet3D, self).__init__()
-        self.pad = (1, 1, 1, 1, 1, 1)
+        up_kernel_size = (2, 2, 2)
+        up_stride = (2, 2, 2)
 
         self.max_pool_2x2x2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
-        self.down_conv1 = self.__double_down_conv(1, 64)
+        self.down_conv_input = nn.Conv3d(1, 8, kernel_size=(3, 3, 3), padding="same", padding_mode="zeros")
+        self.down_conv1 = self.__double_down_conv(8, 64)
         self.down_conv2 = self.__double_down_conv(64, 128)
         self.down_conv3 = self.__double_down_conv(128, 256)
         self.down_conv4 = self.__double_down_conv(256, 512)
 
-        self.up_trans1 = nn.ConvTranspose3d(512, 512, kernel_size=2, stride=2)
+        self.up_trans1 = nn.ConvTranspose3d(512, 512, kernel_size=up_kernel_size, stride=up_stride)
         self.up_conv1 = self.__conv3d(in_channels=768, out_channels=256)
         self.up_conv2 = self.__conv3d(in_channels=256, out_channels=256)
 
-        self.up_trans2 = nn.ConvTranspose3d(256, 256, kernel_size=2, stride=2)
+        self.up_trans2 = nn.ConvTranspose3d(256, 256, kernel_size=up_kernel_size, stride=up_stride)
         self.up_conv3 = self.__conv3d(in_channels=384, out_channels=128)
         self.up_conv4 = self.__conv3d(in_channels=128, out_channels=128)
 
-        self.up_trans3 = nn.ConvTranspose3d(128, 128, kernel_size=2, stride=2)
+        self.up_trans3 = nn.ConvTranspose3d(128, 128, kernel_size=up_kernel_size, stride=up_stride)
         self.up_conv5 = self.__conv3d(in_channels=192, out_channels=64)
         self.up_conv6 = self.__conv3d(in_channels=64, out_channels=64)
-        self.out = nn.Conv3d(in_channels=64, out_channels=n_classes, kernel_size=1)
+        self.out = nn.Conv3d(in_channels=64, out_channels=n_classes, kernel_size=(1, 1, 1))
 
     def forward(self, image):
         # bs, c, d, h, w
         # encoder
-        x1 = self.down_conv1(image)
+        xi = self.down_conv_input(image)
+        x1 = self.down_conv1(xi)
         x2 = self.max_pool_2x2x2(x1)
         x3 = self.down_conv2(x2)
         x4 = self.max_pool_2x2x2(x3)
@@ -147,28 +154,26 @@ class UNet3D(nn.Module):
         x = self.up_trans3(x)
         x = F.relu(self.up_conv5(torch.cat([x1, x], 1)))
         x = F.relu(self.up_conv6(x))
-        x = F.softmax(self.out(x), dim=3)  # ToDo: Be sure if the dim parameter is correct.
+        x = F.softmax(self.out(x), dim=1)  # ToDo: Be sure if the dim parameter is correct.
 
         return x
 
-    def __double_down_conv(self, in_channels, out_channels):
+    @staticmethod
+    def __double_down_conv(in_channels, out_channels, kernel_size_=(3, 3, 3)):
         conv = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels // 2, kernel_size=3, padding="same", padding_mode="zeros"),
+            nn.Conv3d(in_channels, out_channels // 2, kernel_size=kernel_size_, padding="same", padding_mode="zeros"),
             # nn.GroupNorm(4, out_channels // 2),
             nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels // 2, out_channels, kernel_size=3, padding="same", padding_mode="zeros"),
+            nn.Conv3d(out_channels // 2, out_channels, kernel_size=kernel_size_, padding="same", padding_mode="zeros"),
             # nn.GroupNorm(4, out_channels),
             nn.ReLU(inplace=True)
         )
 
         return conv
 
-    def __conv3d(self, in_channels, out_channels):
-        return nn.Conv3d(in_channels=in_channels,
-                         out_channels=out_channels,
-                         kernel_size=3,
-                         padding="same",
-                         padding_mode="zeros")
+    @staticmethod
+    def __conv3d(in_channels, out_channels, kernel_size_=(3, 3, 3)):
+        return nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size_, padding="same", padding_mode="zeros")
 
     # def __crop_img3d(self, tensor, target_tensor):
     #    target_size = target_tensor.size()[2]
