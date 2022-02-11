Huge bug has been fixed. Softmax function added to output!
Former-commit-id: 8d93fdbe5a2b37b6835915ebf326bccc7cdc1e8b [formerly 2854470bb91eac1ade1b2588da076de5ac2629cf]
Former-commit-id: 9cc23e81fe8c0acfcb931d549145438d5f5de201

Code/modules/UNet.py
@@ -104,9 +104,8 @@ class UNet2D(nn.Module):
 
         return source_tensor[:, :, delta:tensor_size - delta, delta:tensor_size - delta]
 
-
 class UNet2Dv2(nn.Module):
-    # Implemented from https://arxiv.org/pdf/1505.04597.pdf
+    # Implemented from https://arxiv.org/pdf/1505.04597.pdf and modified.
     def __init__(self):
         super(UNet2Dv2, self).__init__()
         kernel_size_ = 2
@@ -164,7 +163,7 @@ class UNet2Dv2(nn.Module):
         x = self.up_trans_3(x)
         x = self.up_conv_3(torch.cat([x1, x], 1))
 
-        x = self.out(x)
+        x = F.softmax(self.out(x), dim=1)
 
         return x
 