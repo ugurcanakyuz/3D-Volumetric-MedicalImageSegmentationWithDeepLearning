2a73a820c22b990b7cbf1963cf530af7d3ccb112
Ugurcan Akyuz <ugurcan.akyuz@outlook.com>
2022-02-11 17:46:41 +0300

    Tensorboard modules, new weight init functon, and dice calculation method added.
    Former-commit-id: b6e3a79eb5c91bdc03d34b4f13e29ed192bec870 [formerly 248ba13657098a8b71bc1f7385b585448fa3a431]
    Former-commit-id: 8724a5790a0735b3c6f6c08bbc85caa9d16ba209

--- Code/modules/Utils.py
+++ Code/modules/Utils.py
@@ -1,5 +1,9 @@
 import glob
+
+import numpy as np
 import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
 
 
 def create_patch_indexes(image_shape, patch_sizes):
@@ -91,15 +95,16 @@ def create_onehot_mask(pred_shape, mask, device):
         [bs,number_of_classes,x,y,z]
     """
 
-    mask_onehot = torch.zeros(pred_shape).to(device)
+    mask_onehot = torch.zeros(pred_shape, requires_grad=False).to(device)
     mask = mask.long()
     mask_onehot.scatter_(1, mask, 1)
 
     return mask_onehot
 
 
-def normal_weight_init(m):
+def init_weights_kaiming(m):
     """Initialize model weights with uniform distribution.
+    Find here why kaiming is better for ReLU activations: https://towardsdatascience.com/954fb9b47c79
 
     Parameters
     ----------
@@ -111,4 +116,124 @@ def normal_weight_init(m):
     """
 
     if isinstance(m, torch.nn.Conv2d):
-        torch.nn.init.xavier_uniform_(m.weight)
+        torch.nn.init.kaiming_normal_(m.weight)
+        m.bias.data.fill_(0)
+
+
+def calculate_dice_score(pred, mask):
+    """This method calculates the dice score for each given class.
+
+    Parameters
+    ----------
+    pred: torch.Tensor
+        [bs, number_of_classes, x, y]
+    mask: torch.Tensor
+        [bs, number_of_classes, x, y]
+    Returns
+    -------
+    dice_scores: torch.Tensor
+        [n_classes, dice_scores] Dice scores of the given classes.
+    """
+
+    (bs, n_classes, x, y) = pred.shape
+
+    with torch.no_grad():
+        pred = pred.permute(1, 0, 2, 3).contiguous()
+        pred = pred.view(n_classes, bs * x * y)
+
+        mask = mask.permute(1, 0, 2, 3).contiguous()
+        mask = mask.view(n_classes, bs * x * y)
+
+    intersection = (pred * mask).sum(-1)
+    denominator = (pred + mask).sum(-1)
+
+    dice_scores = (2 * intersection) / denominator
+
+    return dice_scores
+
+
+class TensorboardModules:
+    """This class consists of methods that allow adding data to Tensorboard.
+    """
+
+    def __init__(self, log_dir="events"):
+        """Initialize summary writer.
+
+        Parameters
+        ----------
+        log_dir: str
+            Storage path of the event files.
+        """
+
+        self.writer = SummaryWriter(log_dir)
+
+    def close(self):
+        """Close summary writer."""
+
+        self.writer.close()
+
+    def add_images(self, tag, image, slices):
+        """Adds images to tensorboard.
+
+        Parameters
+        ----------
+        tag: str
+            Data identifier.
+        image: torch.Tensor
+            [x, y, z] shape.
+        slices: dict of ints
+            Start and end point of the image slice, and step size:
+
+        Returns
+        -------
+        None
+        """
+
+        start, end, step = slices
+
+        if type(image) == np.ndarray:
+            image = torch.Tensor(image)
+
+        grid_data = image[:, :, start:end:step].permute(2, 0, 1)
+
+        img_grid = torchvision.utils.make_grid(grid_data.view(10, 1, 256, 256))
+        self.writer.add_image(tag, img_grid)
+
+    def add_graph(self, model, input_size, device):
+        """Creates an input to model and adds model graph to Tensorboard.
+
+        Parameters
+        ----------
+        model: torch.nn.Module or inherited it.
+        input_size: tuple of ints
+        device: str
+            "cpu" or "cuda"
+
+        Returns
+        -------
+        None
+        """
+
+        w, h = input_size
+        inp = torch.rand((1, 1, w, h)).to(device)
+
+        self.writer.add_graph(model, inp)
+
+    def add_loss(self, loss, step):
+        self.writer.add_scalar("Training loss", loss, step)
+
+    def add_dice_score(self, scores, step):
+        """Add dice scores of each class to Tensorboard.
+
+        Parameters
+        ----------
+        scores: torch.Tensor
+        step: int
+
+        Returns
+        -------
+        None
+        """
+
+        for class_id, dice_score in enumerate(scores):
+            self.writer.add_scalar(f"Class {class_id+1} training dice score", dice_score.item(), step)
