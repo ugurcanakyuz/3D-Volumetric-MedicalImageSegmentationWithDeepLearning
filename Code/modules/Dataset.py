f001b18e27dcdb326b8638e69e6ce080fe04e527
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-02-17 23:28:52 +0300

    New dataset creation class has been added.
    Former-commit-id: ac2774f7925ea80c0cc2b57f6cec4dad18d4b531 [formerly ca02b346794066d6d228ec2600181be55db38363]
    Former-commit-id: bc8f8e4452641487612b3d5c9e33fd4bb6227234

--- Code/modules/Dataset.py
+++ Code/modules/Dataset.py
@@ -87,3 +87,76 @@ class FeTADataSet(Dataset):
 
     def __len__(self):
         return self.meta_data.shape[0]
+
+
+class SplitDataSet:
+    """
+    There are 80 MRI images of 80 subjects. Gestational ages of subjects ranges 20 weeks to 35 weeks.
+    There are Pathological and Neurotypical subjects.
+    First 40 MRI images (sub-001 - sub-040) constructed by mialSRTK method.
+    Other 40 MRI images (sub-041 - sub-080) constructed by simpleIRTK method.
+
+    mialSRTK reconstruction:
+        * Gestational age <=28 (28 choosed intuitively for diversity gestational weeks and smoother age disturbition)
+            - Neurotypical: 7 MRI images.   [train:5, val:1, test:1]
+            - Pathological: 20 MRI images.  [train:16, val:2, test:2]
+        * Gestational age > 28
+            - Neurotypical: 8 MRI images.   [train:6, val:1, test:1]
+            - Pathological: 5 MRI images.   [train:3, val:1, test:1]
+
+    simpleIRTK reconstruction:
+        * Gestational age <=28
+            - Neurotypical: 7 MRI images.   [train:5, val:1, test:1]
+            - Pathological: 17 MRI images.  [train:13, val:2, test:2]
+        * Gestational age > 28
+            - Neurotypical: 11 MRI images.  [train:9, val:1, test:1]
+            - Pathological: 5 MRI images.   [train:3, val:1, test:1]
+    """
+
+    def __init__(self, meta_data):
+        mial_srtk = meta_data[:40]
+        simple_irtk = meta_data[40:]
+
+        self.index1 = mial_srtk[
+            (mial_srtk["Gestational age"] <= 28) & (mial_srtk["Pathology"] == "Neurotypical")].index.to_list()
+        self.index2 = mial_srtk[
+            (mial_srtk["Gestational age"] <= 28) & (mial_srtk["Pathology"] == "Pathological")].index.to_list()
+
+        self.index3 = mial_srtk[
+            (mial_srtk["Gestational age"] > 28) & (mial_srtk["Pathology"] == "Neurotypical")].index.to_list()
+        self.index4 = mial_srtk[
+            (mial_srtk["Gestational age"] > 28) & (mial_srtk["Pathology"] == "Pathological")].index.to_list()
+
+        self.index5 = simple_irtk[
+            (simple_irtk["Gestational age"] <= 28) & (simple_irtk["Pathology"] == "Neurotypical")].index.to_list()
+        self.index6 = simple_irtk[
+            (simple_irtk["Gestational age"] <= 28) & (simple_irtk["Pathology"] == "Pathological")].index.to_list()
+
+        self.index7 = simple_irtk[
+            (simple_irtk["Gestational age"] > 28) & (simple_irtk["Pathology"] == "Neurotypical")].index.to_list()
+        self.index8 = simple_irtk[
+            (simple_irtk["Gestational age"] > 28) & (simple_irtk["Pathology"] == "Pathological")].index.to_list()
+
+    def get_train_indexes(self):
+        train = [self.index1[:5], self.index2[:16], self.index3[:6], self.index4[:3], self.index5[:5], self.index6[:13],
+                 self.index7[:9], self.index8[:3]]
+
+        train = [item for sub_arr in train for item in sub_arr]
+
+        return train
+
+    def get_val_indexes(self):
+        validation = [self.index1[5:6], self.index2[16:18], self.index3[6:7], self.index4[3:4], self.index5[5:6],
+                      self.index6[13:15], self.index7[9:10], self.index8[3:4]]
+
+        validation = [item for sub_arr in validation for item in sub_arr]
+
+        return validation
+
+    def get_test_indexes(self):
+        test = [self.index1[6:], self.index2[18:], self.index3[7:], self.index4[4:], self.index5[6:], self.index6[15:],
+                self.index7[10:], self.index8[4:]]
+
+        test = [item for sub_arr in test for item in sub_arr]
+
+        return test
