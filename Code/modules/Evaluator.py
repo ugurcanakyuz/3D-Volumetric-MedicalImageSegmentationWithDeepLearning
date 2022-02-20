c79a628e150f68448a272441d961a8f64c101b0c
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-02-20 13:29:46 +0300

    2D evaluator has been implemeneted.
    Former-commit-id: 4f0d27fe0497ce62b4df6420b2dbd7f170529e52 [formerly 926c0a9843d6d26a816a9e0a9a37508a9306c9eb]
    Former-commit-id: 335151bc7a6f344f80d37a18f15cade14c9988a7

--- Code/modules/Evaluator.py
+++ Code/modules/Evaluator.py
@@ -59,3 +59,54 @@ class Evaluator3D:
             avg_scores = running_dice_scores / len(self.val_loader) * len(self.patch_indexes)
 
         return avg_scores
+
+
+class Evaluator2D:
+    """This class consist of evaluation method. Evaluate method calculates dice score per channel.
+
+    Example:
+        evaluator = Evaluator2D(model, test_loader)
+        avg_score = evaluator.evaluate(model)
+    """
+
+    def __init__(self, model, val_loader):
+        self.val_loader = val_loader
+
+        self.device = next(model.parameters()).device
+
+        out_channels = model.out.out_channels
+        sample = next(iter(val_loader))
+        shape = tuple(sample[0].shape[1:])
+        self.output_shape = (val_loader.batch_size, out_channels, *shape)
+
+    def evaluate(self, model):
+        """Calculates dice score for each class.
+
+        Parameters
+        ----------
+        model: torch.model
+
+        Returns
+        -------
+        avg_scores: float
+            Average dice score for each class.
+        """
+
+        running_dice_scores = torch.zeros(self.output_shape[1]).to(self.device)
+        with torch.no_grad():
+            for j, (image, mask) in enumerate(self.val_loader):
+                image = image.to(self.device)  # [bs,x,y,z]
+                image = image.view(self.output_shape[0], 1, *self.output_shape[2:])  # [bs,c,x,y,z]
+
+                mask = mask.to(self.device)  # [x,y,z]
+                mask = mask.view(self.output_shape[0], 1, *self.output_shape[2:])  # [bs,1,x,y,z]
+
+                output = F.softmax(model(image), dim=1)
+                one_hot_mask = create_onehot_mask(output.shape, mask)
+
+                scores = calculate_dice_score(output, one_hot_mask)
+                running_dice_scores += scores
+
+            avg_scores = running_dice_scores / len(self.val_loader)
+
+        return avg_scores
