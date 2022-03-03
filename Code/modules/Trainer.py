970498334f1a0cabb1197b3c2a0f50e5739e8768
Ugurcan Akyuz <ugurcan.akyuz@outlook.com>
2022-03-03 14:38:26 +0300

    3D Trainer has been implemented.
    Former-commit-id: 62212794d6ae41ddc5689aba5ab2d062fd640917 [formerly 986176af7a425c7b21ad8e468859b8f20ce66802]
    Former-commit-id: b7f17623d99293b89ea1ca0973e0492a16826f15

--- Code/modules/Trainer.py
+++ Code/modules/Trainer.py
@@ -1,6 +1,7 @@
 import tqdm
 import torch
 
+
 class Trainer2D:
     """This class consist of fit method for training. fit method train the model with given data.
 
@@ -35,8 +36,8 @@ class Trainer2D:
         running_dice_scores = torch.zeros(8).to(device)
         bs_2d = 16
 
-
-        prog_bar = tqdm.tqdm(enumerate(self.train_loader), total=int(len(self.train_loader) / self.train_loader.batch_size))
+        prog_bar = tqdm.tqdm(enumerate(self.train_loader),
+                             total=int(len(self.train_loader) / self.train_loader.batch_size))
         prog_bar.set_description(f"Epoch [{self.curr_epoch + 1}/{self.total_epochs}]")
         prog_bar.set_postfix_str(f'Loss: {avg_train_loss}')
 
@@ -68,6 +69,71 @@ class Trainer2D:
                 # Sum losses and dice scores for all predicitions.
                 running_loss += loss.item()
                 count_forward += 1
+
+            avg_train_loss = running_loss / count_forward
+            prog_bar.set_postfix_str(f'Loss: {avg_train_loss:.4f}')
+
+        self.curr_epoch += 1
+
+        if self.scheduler:
+            # Update scheduler.
+            self.scheduler.step()
+
+        return avg_train_loss
+
+
+class Trainer3D:
+    def __init__(self, model, train_loader, optimizer, criterion, patch_indexes, total_epochs, scheduler=None):
+        self.curr_epoch = 0
+
+        self.criterion = criterion
+        self.model = model
+        self.optimizer = optimizer
+        self.patch_indexes = patch_indexes
+        self.scheduler = scheduler
+        self.total_epochs = total_epochs
+        self.train_loader = train_loader
+
+        model.train()
+
+    def fit(self):
+        avg_train_loss = None
+        device = next(self.model.parameters()).device
+        count_forward = 0
+        running_loss = 0.0
+
+        prog_bar = tqdm.tqdm(enumerate(self.train_loader),
+                             total=int(len(self.train_loader) / self.train_loader.batch_size))
+        prog_bar.set_description(f"Epoch [{self.curr_epoch + 1}/{self.total_epochs}]")
+        prog_bar.set_postfix_str(f'Loss: {avg_train_loss}')
+
+        for i, (image, mask) in prog_bar:
+            image = image.to(device)  # [bs,x,y,z]
+            image = image.unsqueeze(1)  # [bs,1,x,y,z]
+
+            mask = mask.to(device)  # [x,y,z]
+            mask = mask.unsqueeze(1)  # [bs,1,x,y,z]
+
+            for coors in self.patch_indexes:
+                [sx, sy, sz] = coors[0]
+                [ex, ey, ez] = coors[1]
+                patch_image = image[:, :, sx:ex, sy:ey, sz:ez]
+                patch_mask = mask[:, :, sx:ex, sy:ey, sz:ez]
+
+                unique, counts = torch.unique(mask[sx:ex, sy:ey, sz:ez], return_counts=True)
+
+                # If the label count below the 70 pixel don't train with it.
+                if torch.any(counts[1:] > 70):
+                    outputs = self.model(patch_image.float())
+
+                    loss = self.criterion(outputs, patch_mask)
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                    # Sum losses and dice scores for all predicitions.
+                    running_loss += loss.item()
+                    count_forward += 1
 
             avg_train_loss = running_loss / count_forward
             prog_bar.set_postfix_str(f'Loss: {avg_train_loss:.4f}')
