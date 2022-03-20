4aca08f14d097664c98002c29262904d48f20db2
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-03-20 15:15:50 +0300

    Larger 3D UNet has been implemented.
    Former-commit-id: b07743f568b1dd278dcd50503af5da2940c40e66 [formerly 90482b139f2a905b66157756c9da11c7029b2b18]
    Former-commit-id: 6ff19347c8c968cc708b93b2778f6b2068737bde

--- Code/modules/UNet.py
+++ Code/modules/UNet.py
@@ -267,3 +267,86 @@ class UNet3D(nn.Module):
         )
 
         return conv
+
+
+class UNet3Dv2(nn.Module):
+    # Implemented from https://arxiv.org/pdf/1606.06650.pdf.
+    def __init__(self, n_classes=8):
+        super(UNet3Dv2, self).__init__()
+        up_kernel_size = (2, 2, 2)
+        up_stride = (2, 2, 2)
+
+        self.max_pool_2x2x2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.down_conv_input = nn.Conv3d(1, 64, kernel_size=(3, 3, 3), padding="same", padding_mode="zeros")
+        self.down_conv1 = self.__double_down_conv(64, 128)
+        self.down_conv2 = self.__double_down_conv(128, 256)
+        self.down_conv3 = self.__double_down_conv(256, 512)
+        self.down_conv4 = self.__double_down_conv(512, 768)
+
+        self.up_trans1 = nn.ConvTranspose3d(768, 768, kernel_size=up_kernel_size, stride=up_stride)
+        self.up_conv1 = self.__double_up_conv(in_channels=1280, out_channels=512)
+
+        self.up_trans2 = nn.ConvTranspose3d(512, 512, kernel_size=up_kernel_size, stride=up_stride)
+        self.up_conv2 = self.__double_up_conv(in_channels=768, out_channels=256)
+
+        self.up_trans3 = nn.ConvTranspose3d(256, 256, kernel_size=up_kernel_size, stride=up_stride)
+        self.up_conv3 = self.__double_up_conv(in_channels=384, out_channels=128)
+
+        self.out = nn.Conv3d(in_channels=128, out_channels=n_classes, kernel_size=(1, 1, 1))
+
+    def forward(self, image):
+        # bs, c, d, h, w
+        # encoder
+        xi = self.down_conv_input(image)
+        x1 = self.down_conv1(xi)
+        x2 = self.max_pool_2x2x2(x1)
+        x3 = self.down_conv2(x2)
+        x4 = self.max_pool_2x2x2(x3)
+        x5 = self.down_conv3(x4)
+        x6 = self.max_pool_2x2x2(x5)
+        x7 = self.down_conv4(x6)
+
+        # decoder
+        x = self.up_trans1(x7)
+        x = torch.cat([x5, x], 1)
+        x = self.up_conv1(x)
+        x = self.up_trans2(x)
+        x = torch.cat([x3, x], 1)
+        x = self.up_conv2(x)
+        x = self.up_trans3(x)
+        x = torch.cat([x1, x], 1)
+        x = self.up_conv3(x)
+        x = self.out(x)
+        # x = F.softmax(x, dim=1)  # ToDo: Be sure if the dim parameter is correct.
+
+        return x
+
+    @staticmethod
+    def __double_down_conv(in_channels, out_channels, kernel_size_=(3, 3, 3)):
+        conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels // 2, kernel_size=kernel_size_, padding="same", padding_mode="zeros"),
+            # nn.BatchNorm3d(out_channels // 2),
+            nn.GroupNorm(8, out_channels // 2),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels // 2, out_channels, kernel_size=kernel_size_, padding="same", padding_mode="zeros"),
+            # nn.BatchNorm3d(out_channels),
+            nn.GroupNorm(8, out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        return conv
+
+    @staticmethod
+    def __double_up_conv(in_channels, out_channels, kernel_size_=(3, 3, 3)):
+        conv = nn.Sequential(
+            nn.Conv3d(in_channels, in_channels // 2, kernel_size=kernel_size_, padding="same", padding_mode="zeros"),
+            # nn.BatchNorm3d(out_channels),
+            nn.GroupNorm(8, in_channels // 2),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(in_channels // 2, out_channels, kernel_size=kernel_size_, padding="same", padding_mode="zeros"),
+            # nn.BatchNorm3d(out_channels),
+            nn.GroupNorm(8, out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        return conv
