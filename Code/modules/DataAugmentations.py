fa6a73e3bd66496f659cb9b6c89c9090c979e8b5
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-05-06 19:17:31 +0300

    Dosctring added.
    Former-commit-id: fbb406822c7bba02b66ad2e051a05fdd9f11f10c [formerly b7768277a1eafedf9c110006170d1be8a45bf802]
    Former-commit-id: ae9815231611b95f97e618bbc9667099591824a8

--- Code/modules/DataAugmentations.py
+++ Code/modules/DataAugmentations.py
@@ -6,6 +6,19 @@ import torchio as tio
 
 
 def apply_transform(mri, mask, transform):
+    """Apply same transform to mri and mask.
+
+    Parameters
+    ----------
+    mri: torch.Tensor
+    mask: torch.Tensor
+    transform: torchio.Transform
+
+    Returns
+    -------
+    mri: torch.Tensor
+    mask: torch.Tensor
+    """
 
     seed = np.random.randint(2147483647)
 
@@ -21,6 +34,12 @@ def apply_transform(mri, mask, transform):
 
 
 class RandomAffine:
+    """Apply a random affine transformation and resample mri and mask.
+    See details: https://torchio.readthedocs.io/transforms/augmentation.html#randomaffine
+    FeTA and dHCP datasets have already transformed to isotropic data with 0.5x0.5x0.5mm resolutions.
+    Therefore, 'isotropic' and 'translation' parameters was set manually.
+    """
+
     def __init__(self, scales=(0.95, 0.95), degrees=0):
         self.degrees = degrees
         self.isotropic = True  # Datasets are isotropic.
@@ -39,6 +58,11 @@ class RandomAffine:
 
 
 class RandomElasticDeformation:
+    """Apply random elastic deformation to mri and mask.
+    See details: https://torchio.readthedocs.io/transforms/augmentation.html#randomelasticdeformation
+    This transformation has high computational cost and can extend the training time.
+    """
+
     def __init__(self, num_control_points=7, locked_borders=2):
         self.num_control_points = num_control_points
         self.locked_borders = locked_borders
@@ -54,6 +78,11 @@ class RandomElasticDeformation:
 
 
 class RandomMotion:
+    """Simulate random motion artifacts for data augmentation to mri and mask.
+    See details: https://torchio.readthedocs.io/transforms/augmentation.html#randommotion
+    !!! Warning !!! This augmentation has not been verified for both mri and mask.
+    """
+
     def __init__(self, degrees=0, num_transforms=2, translations=0.5):
         assert False, "Needs to be fixed. Don't use it."
 
