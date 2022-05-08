27399d6c8370f135888b33954433503a95792197
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-05-08 15:15:34 +0300

    To save mri and mask of its a new method has been implemented.
    Former-commit-id: f2c9e18d06d3916e756f028d39586f2a8ac7c145 [formerly cd8706fb84974cad40e80f3c17a6eb7f00e3231c]
    Former-commit-id: 88f1c689991d7510889cf128d7656ad4445d546e

--- Code/modules/Utils.py
+++ Code/modules/Utils.py
@@ -3,8 +3,79 @@ import os
 import re
 
 import matplotlib.pyplot as plt
+import nibabel as nib
 import numpy as np
 import torch
+
+
+def calculate_dice_score(pred, mask, smooth=1e-5):
+    """This method calculates the dice score for each given class.
+
+    Parameters
+    ----------
+    pred: torch.Tensor
+        [bs, number_of_classes, x, y] or
+        [bs, number_of_classes, x, y, z]
+    mask: torch.Tensor
+        [bs, number_of_classes, x, y] or
+        [bs, number_of_classes, x, y, z]
+    smooth: float
+
+    Returns
+    -------
+    dice_scores: torch.Tensor
+        [n_classes, dice_scores] Dice scores of the given classes.
+    """
+
+    with torch.no_grad():
+        if len(pred.shape) == 4:
+            (bs, n_classes, x, y) = pred.shape
+            pred = pred.permute(1, 0, 2, 3).contiguous()
+            pred = pred.view(n_classes, bs * x * y)
+
+            mask = mask.permute(1, 0, 2, 3).contiguous()
+            mask = mask.view(n_classes, bs * x * y)
+        elif len(pred.shape) == 5:
+            (bs, n_classes, x, y, z) = pred.shape
+            pred = pred.permute(1, 0, 2, 3, 4).contiguous()
+            pred = pred.view(n_classes, bs * x * y * z)
+
+            mask = mask.permute(1, 0, 2, 3, 4).contiguous()
+            mask = mask.view(n_classes, bs * x * y * z)
+
+    intersection = (pred * mask).sum(-1)
+    denominator = (pred + mask).sum(-1)
+
+    dice_scores = ((2 * intersection).clamp(min=smooth)) / denominator.clamp(min=smooth)
+
+    return dice_scores
+
+
+def create_onehot_mask(pred_shape, mask):
+    """Creates onehot mask for multidimensional mask.
+
+    Parameters
+    ----------
+    pred_shape: tuple
+        (bs,number_of_classes,x,y)
+                    or
+        (bs,number_of_classes,x,y,z)
+    mask: torch.Tensor
+        (bs,1,x,y)
+            or
+        (bs,1,x,y,z)
+
+    Returns
+    -------
+    mask_onehot: torch.Tensor
+        [bs,number_of_classes,x,y,z]
+    """
+
+    mask_onehot = torch.zeros(pred_shape, requires_grad=False).to(mask.device)
+    mask = mask.long()
+    mask_onehot.scatter_(1, mask, 1)
+
+    return mask_onehot
 
 
 def create_patch_indexes(image_shape, patch_sizes):
@@ -83,33 +154,6 @@ def get_file_names(path_data):
     return paths
 
 
-def create_onehot_mask(pred_shape, mask):
-    """Creates onehot mask for multidimensional mask.
-
-    Parameters
-    ----------
-    pred_shape: tuple
-        (bs,number_of_classes,x,y)
-                    or
-        (bs,number_of_classes,x,y,z)
-    mask: torch.Tensor
-        (bs,1,x,y)
-            or
-        (bs,1,x,y,z)
-
-    Returns
-    -------
-    mask_onehot: torch.Tensor
-        [bs,number_of_classes,x,y,z]
-    """
-
-    mask_onehot = torch.zeros(pred_shape, requires_grad=False).to(mask.device)
-    mask = mask.long()
-    mask_onehot.scatter_(1, mask, 1)
-
-    return mask_onehot
-
-
 def init_weights_kaiming(m):
     """Initialize model weights with uniform distribution.
     Find here why kaiming is better for ReLU activations: https://towardsdatascience.com/954fb9b47c79
@@ -126,49 +170,6 @@ def init_weights_kaiming(m):
     if isinstance(m, torch.nn.Conv2d):
         torch.nn.init.kaiming_normal_(m.weight)
         m.bias.data.fill_(0)
-
-
-def calculate_dice_score(pred, mask, smooth=1e-5):
-    """This method calculates the dice score for each given class.
-
-    Parameters
-    ----------
-    pred: torch.Tensor
-        [bs, number_of_classes, x, y] or
-        [bs, number_of_classes, x, y, z]
-    mask: torch.Tensor
-        [bs, number_of_classes, x, y] or
-        [bs, number_of_classes, x, y, z]
-    smooth: float
-
-    Returns
-    -------
-    dice_scores: torch.Tensor
-        [n_classes, dice_scores] Dice scores of the given classes.
-    """
-
-    with torch.no_grad():
-        if len(pred.shape) == 4:
-            (bs, n_classes, x, y) = pred.shape
-            pred = pred.permute(1, 0, 2, 3).contiguous()
-            pred = pred.view(n_classes, bs * x * y)
-
-            mask = mask.permute(1, 0, 2, 3).contiguous()
-            mask = mask.view(n_classes, bs * x * y)
-        elif len(pred.shape) == 5:
-            (bs, n_classes, x, y, z) = pred.shape
-            pred = pred.permute(1, 0, 2, 3, 4).contiguous()
-            pred = pred.view(n_classes, bs * x * y * z)
-
-            mask = mask.permute(1, 0, 2, 3, 4).contiguous()
-            mask = mask.view(n_classes, bs * x * y * z)
-
-    intersection = (pred * mask).sum(-1)
-    denominator = (pred + mask).sum(-1)
-
-    dice_scores = ((2 * intersection).clamp(min=smooth)) / denominator.clamp(min=smooth)
-
-    return dice_scores
 
 
 def plot_sub(image, mask, pred_mask=None, fig_size=(13, 13)):
@@ -202,6 +203,39 @@ def plot_sub(image, mask, pred_mask=None, fig_size=(13, 13)):
             plt.imshow(pred_mask)
 
     plt.show()
+
+
+def save_nii(folder, file_name, data):
+    """Saves mri or mask data as nii.gz file. First converts ndarray to Nifti1Image, then save it as nii.gz. file.
+
+    Parameters
+    ----------
+    folder: str
+        What folder data will be saved under.
+    file_name: str
+        File name without '.nii.gz' extension.
+    data: ndarray
+        3 dimension (x, y, z,) MRI or mask data.
+
+    Returns
+    -------
+    None.
+    """
+
+    file_name += ".nii.gz"
+    full_path = os.path.join(folder, file_name)
+
+    # First convert ndarray nibabel format.
+    try:
+        data = nib.Nifti1Image(data, np.eye(4))
+    except Exception as e:
+        print(str(e))
+
+    # Save data in format of Nifti1Image.
+    try:
+        nib.save(data, full_path)
+    except Exception as e:
+        print(str(e))
 
 
 class EarlyStopping:
