86998ff82443a6c3903be909c5ff56347f3c2a6d
Ugurcan Akyuz <ugurcan.akyuz@outlook.com>
2022-06-29 13:03:00 +0300

    2D Evaluator has been updated. Docstrings were added to 3D Evaluator.
    Former-commit-id: fd923319a02e9439eb8d2f5dd4cddce985afb8e3 [formerly e48db1f9e8c17bb2adec4dcfc0375c340669ad51]
    Former-commit-id: eefbfe643db0bd4d7d42efa35208fb7b6b0470f5

--- Code/modules/Evaluator.py
+++ Code/modules/Evaluator.py
@@ -3,31 +3,50 @@ import torch.nn.functional as F
 import torchio as tio
 import tqdm
 
+from torch.utils.data import DataLoader
 from modules.Utils import calculate_dice_score, create_onehot_mask
 
 
 class Evaluator2D:
-    """This class consist of evaluation method. Evaluate method calculates dice score per channel.
+    """Evaluator of 2D data. Loaded 3D MRI data slices into 2D stacks of batches and
+    calculates loss and DSC of batches per class.
 
     Example:
-        evaluator = Evaluator2D(model, test_loader)
-        avg_score = evaluator.evaluate(model)
+        evaluator = Evaluator2D(model, model, val_loader)
+        avg_score = evaluator.evaluate()
     """
 
     def __init__(self, criterion, model, val_loader):
+        """Creates 2D evaluator object.
+
+        Parameters
+        ----------
+        criterion: modules.LossFunctions
+            To calculate loss value of 2D data.
+        model: modules.UNet
+            2D UNet model implemented in UNet.
+        val_loader: DataLoader
+            3D data loader.
+
+        Returns
+        -------
+        None
+        """
+
         self.model = model
         self.val_loader = val_loader
-        self.bs_2d = 16  # Batch size of 2D slices.
-        self.device = next(model.parameters()).device
         self.val_criterion = criterion
+
+        self.bs_2d = 16
+        self.device = next(model.parameters()).device
 
         out_channels = model.out.out_channels
         sample = next(iter(val_loader))
-        shape = tuple(sample[0].shape[1:])
+        shape = tuple(sample['mri']['data'].shape[1:])
         self.output_shape = (val_loader.batch_size, out_channels, *shape)
 
     def evaluate(self):
-        """Calculates dice score for each class.
+        """Calculates loss and dice score for each class.
 
         Parameters
         ----------
@@ -35,72 +54,97 @@ class Evaluator2D:
 
         Returns
         -------
-        avg_scores: float
-            Average dice score for each class.
+        avg_loss: float
+            Average loss of the validation set.
+        dice_scores: Tensor
+            Dice scores of each subject.
         """
 
+        avg_val_loss = None
+        epoch_loss = []
         running_losses = []
-        running_dice_scores = torch.zeros(self.output_shape[1]).to(self.device)
-        count_forward = 0
+        dice_scores = []
+
+        prog_bar = tqdm.tqdm(enumerate(self.val_loader),
+                             total=int(len(self.val_loader) / self.val_loader.batch_size))
+        prog_bar.set_description(f"Validation ")
+        prog_bar.set_postfix_str(f'Loss: {avg_val_loss}')
 
         self.model.eval()
         with torch.no_grad():
-            for j, (image, mask) in enumerate(self.val_loader):
-                image = image.to(self.device)  # [bs,x,y,z]
-                mask = mask.to(self.device)  # [bs,x,y,z] Most likely bs=1.
-                mask = torch.unsqueeze(mask, 1)  # [bs, 1, x,y,z]
-                pred_mask = torch.Tensor().to(self.device)
+            for i, subject in prog_bar:
+                mri = subject['mri']['data'].to(self.device)          # [bs,1,x,y,z], bs=1 due to memory limit
+                mask = subject['mask']['data'].to(self.device)        # [bs,1,x,y,z], bs=1 due to memory limit
 
-                for slice_ix in range(0, image.shape[1], self.bs_2d):
+                # Slice 3D image. It's like splitting 3D images into batches.
+                for slice_ix in range(0, mri.shape[2], self.bs_2d):
                     start = slice_ix
                     stop = slice_ix + self.bs_2d
 
-                    if stop > image.shape[1]:
-                        stop = image.shape[1]
+                    if stop > mri.shape[2]:
+                        stop = mri.shape[2]
 
-                    slice_image = image[:, start:stop]
-                    slice_image = slice_image.view(-1, 1, 256, 256)
+                    stack_mri = mri[:, :, start:stop].squeeze(0)        # start:stop, For example 0:16, 16:32, 32:64 .
+                    stack_mask = mask[:, :, start:stop].squeeze(0)
+                    stack_mri = stack_mri.permute(1, 0, 2, 3)           # [bs_2d, 1, y,z]
+                    stack_mask = stack_mask.permute(1, 0, 2, 3)         # [bs_2d, 1, y,z]
 
-                    output = self.model(slice_image)
-                    pred_mask = torch.cat((pred_mask, output))
+                    output = self.model(stack_mri.float())
 
-                pred_mask = pred_mask.view(mask.shape[0], *pred_mask.shape)  # [bs_3d, bs_2d, n_c, x, y]
-                pred_mask = pred_mask.permute(0, 2, 1, 3, 4).contiguous()   # [bs_3d, n_c, x(bs_2d), y, z]
+                    # Criterion accepts raw logits so softmax has not been applied to predicted mask yet.
+                    val_loss = self.val_criterion(output, stack_mask)
+                    running_losses.append(val_loss.item())
 
-                val_loss = self.val_criterion(pred_mask, mask)
-                running_losses.append(val_loss.item())
+                    # To calculate Dice Score get softmax applied predicted mask.
+                    pred_mask = F.softmax(output, dim=1)
 
-                # To calculate DS, convert raw model outputs to softmax outputs.
-                pred_mask = F.softmax(pred_mask, dim=1)
+                    # Create one hot encoded mask of original mask.
+                    one_hot_mask = create_onehot_mask(pred_mask.shape, stack_mask)
 
-                # Create one hot encoded mask of original mask.
-                one_hot_mask = create_onehot_mask(pred_mask.shape, mask)
+                    # Convert class probabilities to actual class labels.
+                    pred_mask = torch.argmax(pred_mask, dim=1, keepdim=True)
 
-                # Convert class probabilities to actual class labels.
-                pred_mask = torch.argmax(pred_mask, dim=1, keepdim=True)
+                    # Create one hot encoded mask of predicted mask.
+                    pred_mask = create_onehot_mask(one_hot_mask.shape, pred_mask)
 
-                # Create one hot encoded mask of predicted mask.
-                pred_mask = create_onehot_mask(one_hot_mask.shape, pred_mask)
+                    scores = calculate_dice_score(pred_mask, one_hot_mask)
+                    dice_scores.append(scores)
 
-                scores = calculate_dice_score(pred_mask, one_hot_mask)
-                running_dice_scores += scores
-                count_forward += 1
+                avg_loss = sum(running_losses) / len(running_losses)
+                running_losses = []
+                epoch_loss.append(avg_loss)
+                prog_bar.set_postfix_str(f'Loss: {sum(epoch_loss) / len(epoch_loss):.4f}')
 
-            avg_loss = sum(running_losses) / len(running_losses)
-            avg_scores = running_dice_scores / count_forward
+            avg_loss = sum(epoch_loss) / len(epoch_loss)
 
-        return avg_loss, avg_scores
+        return avg_loss, dice_scores
 
 
 class Evaluator3D:
     """This class consist of evaluation method. Evaluate method calculates dice score per channel.
 
     Example:
-        evaluator = Evaluator3D(model, patch_indexes, test_loader)
-        avg_score = evaluator.evaluate(model)
+        evaluator = Evaluator3D(model, model, val_loader)
+        avg_score = evaluator.evaluate()
     """
 
     def __init__(self, criterion, model, patch_size, val_loader):
+        """Creates 3D evaluator object.
+
+        Parameters
+        ----------
+        criterion: modules.LossFunctions
+            To calculate loss value of 3D data.
+        model: modules.UNet
+            3D UNet model implemented in UNet.
+        val_loader: DataLoader
+            3D data loader.
+
+        Returns
+        -------
+        None
+        """
+
         self.model = model
         self.patch_size = patch_size
         self.val_loader = val_loader
@@ -199,6 +243,7 @@ class Evaluator3D:
         Returns
         -------
         output: torch.Tensor
+            Logits, not the actual class labels of Softmax function.
         """
 
         self.model.eval()
