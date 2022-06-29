71fd46148b9923a28af46c4ec2c6d297ac6fa26f
Ugurcan Akyuz <ugurcan.akyuz@outlook.com>
2022-06-29 13:01:24 +0300

    2D Trainer has been updated. Docstrings were added to 3D Trainer.
    Former-commit-id: 7bdd0ebec8ade52728ab752592684856d52e4a3b [formerly c89d4e436613cb08bc9eea5c4f940c4a44937252]
    Former-commit-id: 9b47efd62754ac72c2b3a69892495c3ef106d82e

--- Code/modules/Trainer.py
+++ Code/modules/Trainer.py
@@ -1,14 +1,40 @@
+import torch
+from torch.utils.data import DataLoader
 import tqdm
 
 
 class Trainer2D:
-    """This class consist of fit method for training. fit method train the model with given data.
+    """Trainer for 2D data and models. Loaded 3D MRI data are sliced into 2D stacks of batches
+    and loss are calculated in the fit() method.
 
     Example:
-        trainer = Trainer2D(model, train_loader, optimizer, criterion, num_epochs, scheduler)
-        avg_train_loss = trainer.fit()
+        trainer = Trainer2D(criterion, model, optimizer, total_epochs, train_loader)
+        avg_loss = trainer.fit()
     """
-    def __init__(self, model, train_loader, optimizer, criterion, total_epochs, scheduler=None):
+
+    def __init__(self, criterion, model, optimizer, total_epochs, train_loader, scheduler=None):
+        """Creates 2D Trainer object.
+
+        Parameters
+        ----------
+        criterion: modules.LossFunction
+            To calculate loss value of 2D data.
+        model: modules.UNet
+            2D UNet model implemented in UNet.
+        optimizer: torch.optim.SGD or others
+            Optimizer object of the torch.optim module.
+        total_epochs: int
+            Total epoch counts of the training.
+        train_loader: DataLoader
+            3D data loader.
+        scheduler: torch.optim.lr_scheduler.StepLR
+            Learning rate scheduler object of the torch.optim.lr_scheduler module.
+
+        Returns
+        -------
+        None
+        """
+
         self.curr_epoch = 0
 
         self.criterion = criterion
@@ -19,56 +45,54 @@ class Trainer2D:
         self.train_loader = train_loader
 
     def fit(self):
-        """Trains the model with given data.
+        """Fit the model to the 2D data.
 
         Returns
         -------
-        avg_train_loss: float
+        float
+            Average loss value of the epoch.
         """
 
-        avg_train_loss = None
-        device = next(self.model.parameters()).device
-        count_forward = 0
-        running_loss = 0.0
+        avg_loss = None
         bs_2d = 16
+        device = next(self.model.parameters()).device
+        epoch_loss = []
+        running_loss = []  # This is only for loss indicator.
 
         prog_bar = tqdm.tqdm(enumerate(self.train_loader),
-                             total=int(len(self.train_loader) / self.train_loader.batch_size))
+                             total=int((len(self.train_loader)) / self.train_loader.batch_size))
         prog_bar.set_description(f"Epoch [{self.curr_epoch + 1}/{self.total_epochs}]")
-        prog_bar.set_postfix_str(f'Loss: {avg_train_loss}')
+        prog_bar.set_postfix_str(f'Loss: {avg_loss}')
 
         self.model.train()
-        for i, (image, mask) in prog_bar:
-            image = image.to(device)  # [bs,x,y,z]
-            mask = mask.to(device)  # [x,y,z]
+        for i, subject in prog_bar:
+            mri = subject['mri']['data'].to(device)              # [bs,1,x,y,z], bs=1 due to memory limit
+            mask = subject['mask']['data'].to(device)            # [bs,1,x,y,z], bs=1 due to memory limit
 
             # Slice 3D image. It's like splitting 3D images into batches.
-            for slice_ix in range(0, image.shape[1], bs_2d):
+            for slice_ix in range(0, mri.shape[2], bs_2d):
                 start = slice_ix
                 stop = slice_ix + bs_2d
 
-                if stop > image.shape[1]:
-                    stop = image.shape[1]
+                if stop > mri.shape[2]:
+                    stop = mri.shape[2]
 
-                slice_image = image[:, start:stop]
-                slice_mask = mask[:, start:stop]
-                slice_image = slice_image.view(-1, 1, *slice_image.shape[-2:])
-                slice_mask = slice_mask.view(-1, 1, *slice_mask.shape[-2:])
+                stack_mri = mri[:, :, start:stop].squeeze(0)
+                stack_mask = mask[:, :, start:stop].squeeze(0)
+                stack_mri = stack_mri.permute(1, 0, 2, 3)               # [bs_2d, 1, y,z]
+                stack_mask = stack_mask.permute(1, 0, 2, 3)             # [bs_2d, 1, y,z]
 
-                outputs = self.model(slice_image.float())
-
-                loss = self.criterion(outputs, slice_mask)
-
+                outputs = self.model(stack_mri.float())
+                loss = self.criterion(outputs, stack_mask)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+                running_loss.append(loss.item())
 
-                # Sum losses and dice scores for all predictions.
-                running_loss += loss.item()
-                count_forward += 1
-
-            avg_train_loss = running_loss / count_forward
-            prog_bar.set_postfix_str(f'Loss: {avg_train_loss:.4f}')
+            avg_loss = sum(running_loss) / len(running_loss)
+            running_loss = []
+            epoch_loss.append(avg_loss)
+            prog_bar.set_postfix_str(f'Loss: {sum(epoch_loss) / len(epoch_loss):.4f}')
 
         self.curr_epoch += 1
 
@@ -76,22 +100,58 @@ class Trainer2D:
             # Update scheduler.
             self.scheduler.step()
 
-        return avg_train_loss
+        return sum(epoch_loss) / len(epoch_loss)
 
 
 class Trainer3D:
-    def __init__(self, criterion, model, optimizer, patch_size, total_epochs, train_loader, scheduler=None):
+    """Trainer for 3D data and models.
+
+    Example:
+        trainer = Trainer2D(criterion, model, optimizer, total_epochs, train_loader)
+        avg_loss = trainer.fit()
+    """
+
+    def __init__(self, criterion, model, optimizer, total_epochs, train_loader, scheduler=None):
+        """Creates 2D Trainer object.
+
+        Parameters
+        ----------
+        criterion: modules.LossFunction
+            To calculate loss value of 2D data.
+        model: modules.UNet
+            3D UNet model implemented in UNet.
+        optimizer: torch.optim.SGD or others
+            Optimizer object of the torch.optim module.
+        total_epochs: int
+            Total epoch counts of the training.
+        train_loader: DataLoader
+            3D data loader.
+        scheduler: torch.optim.lr_scheduler.StepLR
+            Learning rate scheduler object of the torch.optim.lr_scheduler module.
+
+        Returns
+        -------
+        None
+        """
+
         self.curr_epoch = 0
 
         self.criterion = criterion
         self.model = model
         self.optimizer = optimizer
-        self.patch_size = patch_size
         self.scheduler = scheduler
         self.total_epochs = total_epochs
         self.train_loader = train_loader
 
     def fit(self):
+        """Fit the model to the 3D data.
+
+        Returns
+        -------
+        float
+            Average loss value of the epoch.
+        """
+
         avg_loss = None
         device = next(self.model.parameters()).device
         epoch_loss = []
