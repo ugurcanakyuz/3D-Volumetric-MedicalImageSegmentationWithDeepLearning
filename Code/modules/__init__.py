0555a2d39ca1f5989404d351acc2f9bfda38bfa0
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-11-20 13:48:07 +0300

    New modules added.
    Former-commit-id: 076604b5e057643f47f6616fd220d258198837aa [formerly a2fa8519f04e080e42307d0e1514f702ac8e8d8b]
    Former-commit-id: 2b71f9018429ef40ac6e0dc6f3321abf7e65e442

--- Code/modules/__init__.py
+++ Code/modules/__init__.py
@@ -1,8 +1,7 @@
 from .Config import Config
-from .Transforms import *
 from .Dataset import MRIDataset, MRIDatasets
 from .Evaluator import Evaluator2D, Evaluator3D
-from .Models import SDUNet3D
 from .Tensorboard import TensorboardModules
 from .Trainer import Trainer2D, Trainer3D
-from .UNet import UNet2D, UNet3D
+from .Transforms import *
+from .models import *
---
+++ Code/modules/models/__init__.py
@@ -0,0 +1,5 @@
+from .SDUNet3D import SDUNet3D
+from .UNet2D import UNet2D
+from .UNet2Dv2 import UNet2Dv2
+from .UNet3D import UNet3D
+from .UNet3Dv2 import UNet3Dv2
