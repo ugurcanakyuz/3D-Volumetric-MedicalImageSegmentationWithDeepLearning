2b057e30bb61b62af5a3789f19fc955773ad1347
ugurcanakyuz <ugurcanakyuz@outlook.com>
2022-11-27 18:45:11 +0300

    Predictor class implemented.
    Former-commit-id: 0e673f6db448ded8eeedc4719e18f1d0fad65808 [formerly eee3d1bb430655917b1bf6c96827dbdf42a3afae]
    Former-commit-id: 08d896200629c3e162419cfc6c2496ea24e383f2

--- Code/modules/__init__.py
+++ Code/modules/__init__.py
@@ -1,6 +1,7 @@
 from .Config import Config
 from .Dataset import MRIDataset, MRIDatasets
 from .Evaluator import Evaluator2D, Evaluator3D
+from .Predictor import Predictor
 from .Tensorboard import TensorboardModules
 from .Trainer import Trainer2D, Trainer3D
 from .Transforms import *
