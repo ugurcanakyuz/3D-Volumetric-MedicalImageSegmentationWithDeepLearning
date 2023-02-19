077102a828c374bb2f46658e32373d5ea0abb79a
ugurcanakyuz <ugurcanakyuz@outlook.com>
2023-02-19 17:09:21 +0300

    3D SDU-Net architecture fixed.
    Former-commit-id: 3df0b133c41983ffbaa386c7397538fc02b5407a [formerly 1a4756855233da36e9c77319459af0362457311c]
    Former-commit-id: d819d9334da97cf082ab57580c98292d9c9030f5

--- Code/modules/models/SDUNet3D.py
+++ Code/modules/models/SDUNet3D.py
@@ -8,23 +8,33 @@ class DilatedBlock(nn.Module):
                  padding_="same", padding_mode_="zeros"):
         super(DilatedBlock, self).__init__()
 
+
+
         self.conv1 = nn.Conv3d(in_channels, out_channels // 2,
                                kernel_size=kernel_size_, padding=padding_, padding_mode=padding_mode_, dilation=1)
-        self.conv2 = nn.Conv3d(in_channels, out_channels // 4,
+        self.gn1 = nn.GroupNorm(8, out_channels // 2)
+        self.conv2 = nn.Conv3d(out_channels // 2, out_channels // 4,
                                kernel_size=kernel_size_, padding="same", padding_mode="zeros", dilation=3)
-        self.conv3 = nn.Conv3d(in_channels, out_channels // 8,
+        self.gn2 = nn.GroupNorm(8, out_channels // 4)
+        self.conv3 = nn.Conv3d(out_channels // 4, out_channels // 8,
                                kernel_size=kernel_size_, padding="same", padding_mode="zeros", dilation=6)
-        self.conv4 = nn.Conv3d(in_channels, out_channels // 16,
+        self.gn3 = nn.GroupNorm(4, out_channels // 8)
+        self.conv4 = nn.Conv3d(out_channels // 8, out_channels // 16,
                                kernel_size=kernel_size_, padding="same", padding_mode="zeros", dilation=9)
-        self.conv5 = nn.Conv3d(in_channels, out_channels // 16,
+        self.gn4 = nn.GroupNorm(4, out_channels // 16)
+        self.conv5 = nn.Conv3d(out_channels // 16, out_channels // 16,
                                kernel_size=kernel_size_, padding="same", padding_mode="zeros", dilation=12)
 
     def forward(self, data):
-        out1 = F.relu(self.conv1(data))
-        out2 = F.relu(self.conv2(data))
-        out3 = F.relu(self.conv3(data))
-        out4 = F.relu(self.conv4(data))
-        out5 = F.relu(self.conv5(data))
+        out1 = self.conv1(data)
+        x = F.relu(self.gn1(out1))
+        out2 = self.conv2(x)
+        x = F.relu(self.gn2(out2))
+        out3 = self.conv3(x)
+        x = F.relu(self.gn3(out3))
+        out4 = self.conv4(x)
+        x = F.relu(self.gn4(out4))
+        out5 = F.relu(self.conv5(x))
 
         out_map = torch.concat([out1, out2, out3, out4, out5], dim=1)
 
@@ -38,22 +48,23 @@ class SDUNet3D(nn.Module):
         up_stride = (2, 2, 2)
 
         self.max_pool_2x2x2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
-        self.down_conv1 = self.__conv(1, 64)
-        self.down_conv2 = self.__conv(64, 128)
-        self.down_conv3 = self.__conv(128, 256)
-        self.down_conv4 = self.__conv(256, 512)
+        self.down_conv1 = self.__SDBlock(1, 64)
+        self.down_conv2 = self.__SDBlock(64, 128)
+        self.down_conv3 = self.__SDBlock(128, 256)
+        self.down_conv4 = self.__SDBlock(256, 512)
+        self.down_conv5 = self.__SDBlock(512, 512)
 
         self.up_trans1 = nn.ConvTranspose3d(512, 512, kernel_size=up_kernel_size, stride=up_stride)
-        self.up_conv1 = self.__conv(in_channels=1024, out_channels=512)
+        self.up_conv1 = self.__SDBlock(in_channels=1024, out_channels=256)
 
-        self.up_trans2 = nn.ConvTranspose3d(512, 512, kernel_size=up_kernel_size, stride=up_stride)
-        self.up_conv2 = self.__conv(in_channels=768, out_channels=384)
+        self.up_trans2 = nn.ConvTranspose3d(256, 256, kernel_size=up_kernel_size, stride=up_stride)
+        self.up_conv2 = self.__SDBlock(in_channels=512, out_channels=128)
 
-        self.up_trans3 = nn.ConvTranspose3d(384, 384, kernel_size=up_kernel_size, stride=up_stride)
-        self.up_conv3 = self.__conv(in_channels=512, out_channels=128)
+        self.up_trans3 = nn.ConvTranspose3d(128, 128, kernel_size=up_kernel_size, stride=up_stride)
+        self.up_conv3 = self.__SDBlock(in_channels=256, out_channels=64)
 
-        self.up_trans4 = nn.ConvTranspose3d(128, 128, kernel_size=up_kernel_size, stride=up_stride)
-        self.up_conv4 = self.__conv(in_channels=192, out_channels=64)
+        self.up_trans4 = nn.ConvTranspose3d(64, 64, kernel_size=up_kernel_size, stride=up_stride)
+        self.up_conv4 = self.__SDBlock(in_channels=128, out_channels=64)
 
         self.out = nn.Conv3d(in_channels=64, out_channels=n_classes, kernel_size=(1, 1, 1))
 
@@ -68,9 +79,10 @@ class SDUNet3D(nn.Module):
         x6 = self.max_pool_2x2x2(x5)
         x7 = self.down_conv4(x6)
         x8 = self.max_pool_2x2x2(x7)
+        x9 = self.down_conv5(x8)
 
         # decoder
-        x = self.up_trans1(x8)
+        x = self.up_trans1(x9)
         x = torch.cat([x7, x], 1)
         x = self.up_conv1(x)
         x = self.up_trans2(x)
@@ -88,7 +100,7 @@ class SDUNet3D(nn.Module):
         return x
 
     @staticmethod
-    def __conv(in_channels, out_channels):
+    def __SDBlock(in_channels, out_channels):
         conv = nn.Sequential(
             DilatedBlock(in_channels, out_channels),
             nn.GroupNorm(8, out_channels),
