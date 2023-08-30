07397befa3865346678cc7acb2f79d1c59778e41
ugurcanakyuz <ugurcanakyuz@outlook.com>
2023-08-30 19:00:11 +0300

    Config class is used to manage model training by yaml files.
    Former-commit-id: f6ec7e69ac9490ceb94e49458f379bf48505fe09 [formerly 57fa4689fe1453745f2e187de3352bc84adafba9]
    Former-commit-id: 200524c11b2a767ef386d385845f48ee49c8054d

---
+++ src/utils/Config.py
@@ -0,0 +1,87 @@
+from types import SimpleNamespace
+import yaml
+
+
+class NestedNamespace(SimpleNamespace):
+    """A class that extends `SimpleNamespace` to support nested attribute assignment using dictionaries.
+
+    This class allows you to create a nested namespace by providing a dictionary. Each key in the dictionary
+    becomes an attribute of the object, and if the corresponding value is also a dictionary, it is converted
+    into a nested `NestedNamespace` object.
+
+    Notes
+    -----
+    Implemented from https://stackoverflow.com/a/54332748/13080899 .
+
+    Parameters
+    ----------
+    dictionary : dict
+        The dictionary containing the attributes and their values for the namespace.
+    **kwargs
+        Additional keyword arguments to pass to the base class `SimpleNamespace`.
+
+    Examples
+    --------
+    dictionary = {'a': 1, 'b': {'c': 2}}
+    ns = NestedNamespace(dictionary)
+    print(ns.a)  # Output: 1
+    print(ns.b.c)  # Output: 2
+    """
+
+    def __init__(self, dictionary, **kwargs):
+        super().__init__(**kwargs)
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                self.__setattr__(key, NestedNamespace(value))
+            else:
+                self.__setattr__(key, value)
+
+
+class Config(NestedNamespace):
+    """A configuration class that inherits from `NestedNamespace`.
+
+    This class is designed to load configuration data from a YAML file and create a nested namespace
+    object containing the configuration values. It provides an easy way to access configuration options
+    using dot notation.
+
+    Parameters
+    ----------
+    path : str, optional
+        The path to the YAML configuration file. Default is "config.yaml".
+    **kwargs
+        Additional keyword arguments to pass to the base class `NestedNamespace`.
+
+    Examples
+    --------
+    Assuming a YAML file named "config.yaml" with the following content:
+    ```
+    a: 1
+    b:
+        c: 2
+    ```
+    You can create a `Config` object as follows:
+    ```
+    config = Config()
+    print(config.a)  # Output: 1
+    print(config.b.c)  # Output: 2
+    ```
+    """
+
+    def __init__(self, path="config.yaml", **kwargs):
+        """Load configuration data from a YAML file and create a nested namespace.
+
+        Parameters
+        ----------
+        path : str, optional
+            The path to the YAML configuration file. Default is "config.yaml".
+        **kwargs
+            Additional keyword arguments to pass to the base class `NestedNamespace`.
+        """
+
+        with open(path, "r") as stream:
+            try:
+                config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        super().__init__(config)
